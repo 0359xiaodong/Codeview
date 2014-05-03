0d6fec57b84be253c26e6add1a94fd7732610ba1
newmlee <matthewlee0725@gmail.com>
2014-05-03 17:41:11 +0000

    更新

--- src/io/github/mthli/Codeview/LoginActivity.java
+++ src/io/github/mthli/Codeview/LoginActivity.java
@@ -2,6 +2,8 @@ package io.github.mthli.Codeview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class LoginActivity extends Activity {
     /**
@@ -11,5 +13,16 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    /**
+     * To show the menu in ActionBar,
+     * we need to create this method
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.login, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

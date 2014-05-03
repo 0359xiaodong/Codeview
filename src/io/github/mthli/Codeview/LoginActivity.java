31b9ceda0626074d23b9f8f36b65989dad65d5db
newmlee <matthewlee0725@gmail.com>
2014-05-03 17:09:17 +0000

    更新

---
+++ src/io/github/mthli/Codeview/LoginActivity.java
@@ -0,0 +1,15 @@
+package io.github.mthli.Codeview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}

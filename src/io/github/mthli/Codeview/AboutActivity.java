eee06f24e8d59f5990d02dee900cf82ac414b9af
newmlee <matthewlee0725@gmail.com>
2014-05-06 09:20:14 +0000

    重构

---
+++ src/io/github/mthli/Codeview/AboutActivity.java
@@ -0,0 +1,49 @@
+package io.github.mthli.Codeview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.*;
+
+public class AboutActivity extends Activity {
+    public void onCreate(Bundle saved_instance_state) {
+        super.onCreate(saved_instance_state);
+        setContentView(R.layout.list_view_about);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ArrayList<String> title = new ArrayList<String>();
+        title.add(getString(R.string.about_title_designer));
+        title.add(getString(R.string.about_title_coder));
+        title.add(getString(R.string.about_title_version));
+        title.add(getString(R.string.about_title_homepage));
+        title.add(getString(R.string.about_title_license));
+
+        ArrayList<String> content = new ArrayList<String>();
+        content.add(getString(R.string.about_content_designer));
+        content.add(getString(R.string.about_content_coder));
+        content.add(getString(R.string.about_content_version));
+        content.add(getString(R.string.about_content_homepage));
+        content.add(getString(R.string.about_content_license));
+
+        List<Map<String, String>> list_view_items = new ArrayList<Map<String, String>>();
+
+        for (int i = 0; i < 5; i++) {
+            Map<String, String> list_view_item = new HashMap<String, String>();
+            list_view_item.put("title", title.get(i));
+            list_view_item.put("content", content.get(i));
+            list_view_items.add(list_view_item);
+        }
+
+        SimpleAdapter simple_adapter = new SimpleAdapter(
+                this,
+                list_view_items,
+                R.layout.list_view_item_about,
+                new String[] {"title", "content"},
+                new int[] {R.id.list_view_item_about_title, R.id.list_view_item_about_content}
+        );
+        ListView list_view = (ListView) findViewById(R.id.list_view_about);
+        list_view.setAdapter(simple_adapter);
+    }
+}
---
+++ src/io/github/mthli/Codeview/MainActivity.java
@@ -0,0 +1,36 @@
+package io.github.mthli.Codeview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                Intent intent_about = new Intent(this, AboutActivity.class);
+                startActivity(intent_about);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}

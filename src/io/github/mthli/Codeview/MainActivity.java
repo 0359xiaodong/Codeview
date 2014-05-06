a062948cf9cea24589af57ed7080078b323aa96d
newmlee <matthewlee0725@gmail.com>
2014-05-06 15:29:35 +0000

    更新

--- src/io/github/mthli/Codeview/MainActivity.java
+++ src/io/github/mthli/Codeview/MainActivity.java
@@ -1,18 +1,45 @@
 package io.github.mthli.Codeview;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity
+        extends Activity
+        implements ActionBar.OnNavigationListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ActionBar action_bar = getActionBar();
+        action_bar.setDisplayShowTitleEnabled(false);
+
+        action_bar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        action_bar.setListNavigationCallbacks(
+                new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        new String[] {
+                                getString(R.string.action_bar_drop_down_repo),
+                                getString(R.string.action_bar_drop_down_mark)
+                        }
+                ),
+                this
+        );
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int i, long j) {
+        System.out.println("jdish");
+        return true;
     }
 
     @Override

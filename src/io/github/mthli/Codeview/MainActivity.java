535ec0629d8226d5fca8b93f579d82319b503775
newmlee <matthewlee0725@gmail.com>
2014-05-07 09:46:04 +0000

    更新

--- src/io/github/mthli/Codeview/MainActivity.java
+++ src/io/github/mthli/Codeview/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.SearchView;
 
 public class MainActivity
         extends Activity

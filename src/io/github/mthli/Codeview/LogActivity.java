c8b5344e13c6b5f2f2a4c5648654d1c6d44ead6e
newmlee <matthewlee0725@gmail.com>
2014-05-10 09:42:24 +0000

    更新

---
+++ src/io/github/mthli/Codeview/LogActivity.java
@@ -0,0 +1,21 @@
+package io.github.mthli.Codeview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LogActivity extends Activity {
+    public void onCreate(Bundle saved_instance_state) {
+        super.onCreate(saved_instance_state);
+        setContentView(R.layout.list_view_log);
+        getActionBar().setDisplayShowHomeEnabled(true);
+
+        /*
+         * Also we need to implement something here,
+         * such as setTitle() and so on.
+         */
+        Intent intent_log = getIntent();
+        String get_title = intent_log.getStringExtra("item_title");
+        getActionBar().setTitle(get_title);
+    }
+}
--- src/io/github/mthli/Codeview/SwipeListItemAdapter.java
+++ src/io/github/mthli/Codeview/SwipeListItemAdapter.java
@@ -2,6 +2,7 @@ package io.github.mthli.Codeview;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,10 +66,49 @@ public class SwipeListItemAdapter extends ArrayAdapter<SwipeListItem> {
             holder = (NewHolder) view.getTag();
         }
 
-        SwipeListItem an_item =  swipe_list_item.get(position);
+        ((SwipeListView)parent).recycle(view, position); // Maybe useless.
+
+        final SwipeListItem an_item =  swipe_list_item.get(position);
         holder.item_image.setImageDrawable(an_item.getItemImage());
         holder.item_title.setText(an_item.getItemTitle());
         holder.item_content.setText(an_item.getItemContent());
+
+        /* Now back button setting */
+        holder.button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(context, "Delete needs to be implemented.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Refresh needs to be implemented.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Mark needs to be implemented.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(context, "Log needs to be implemented.", Toast.LENGTH_SHORT).show();
+                // Intent intent_log = new Intent(context, LogActivity.class);
+                // context.startActivity(intent_log);
+
+                String get_title = an_item.getItemTitle();
+
+                Intent intent_log = new Intent(context, LogActivity.class);
+                intent_log.putExtra("item_title", get_title);
+                context.startActivity(intent_log);
+            }
+        });
 
         return view;
     }
